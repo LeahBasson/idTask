14ce0a6ec3f2a0f837ed48e6caa31c89751b6303
LeahBasson <leahbasson54@gmail.com>
2024-05-23 16:28:39 +0200

    id Task

---
+++ script/code.js
@@ -0,0 +1,13 @@
+
+let Submit = document.querySelector('[Submit]');
+
+Submit.addEventListener('click', () => {
+    let idNumber = document.querySelector('[idNumber]').value;
+    let gender = idNumber.slice(5, 9);
+    let condition = (gender < 5000) ? 'Male' : 'Female';
+
+    if (!isNaN(idNumber) && idNumber.length == 13){
+    document.querySelector('[dob]').textContent =  (`Date of birth: ${idNumber.slice(0, 6)}`);
+    document.querySelector('[ genderDisplay]').textContent = (`Gender: ${condition}`);
+    }
+})
