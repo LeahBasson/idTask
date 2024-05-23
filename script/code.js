48023c9dca318947b8c4e5ab5eb2671236980b07
LeahBasson <leahbasson54@gmail.com>
2024-05-23 16:33:30 +0200

    Complete

--- script/code.js
+++ script/code.js
@@ -10,4 +10,7 @@ Submit.addEventListener('click', () => {
     document.querySelector('[dob]').textContent =  (`Date of birth: ${idNumber.slice(0, 6)}`);
     document.querySelector('[ genderDisplay]').textContent = (`Gender: ${condition}`);
     }
+    else{
+        document.querySelector('[invalid]').textContent = (`Your Id number is invalid`);
+    }
 })
